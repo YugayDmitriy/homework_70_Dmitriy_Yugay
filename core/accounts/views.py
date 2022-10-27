158ffbdf656caa534701b66b4bae026468f02e66
Dmitriy <Dmitrijugaj614@gmail.com>
2022-10-27 14:19:54 +0600

    Добавлен файд пользователя

---
+++ core/accounts/views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
+from accounts.forms import LoginForm
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+class LoginView(SuccessMessageMixin, TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+    success_message = "Вход пользователя %(username)s успешно совершен"
+
+    def get(self, request, *args, **kwargs):
+        next = request.GET.get('next')
+        form_data = {} if not next else {'next': next}
+        form = self.form(form_data)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('login')
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        next = form.cleaned_data.get('next')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return redirect('login')
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('index_projects')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('index_projects')
