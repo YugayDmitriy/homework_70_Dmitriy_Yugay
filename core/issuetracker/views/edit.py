a9c3efb41c8461f4cabf18585b2e93038f34cb43
Dmitriy <Dmitrijugaj614@gmail.com>
2022-10-27 15:10:11 +0600

    Внесены ограничения для не авторизованных пользователей

--- core/issuetracker/views/edit.py
+++ core/issuetracker/views/edit.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.views.generic import UpdateView
 from issuetracker.forms import IssueForm
@@ -5,7 +6,7 @@ from issuetracker.models import Issue
 from django.contrib.messages.views import SuccessMessageMixin
 
 
-class IssueEditView(SuccessMessageMixin, UpdateView):
+class IssueEditView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     template_name = 'edit.html'
     form_class = IssueForm
     model = Issue
